14344235bc979b9a1d362c651a9767bc4948e0f1
Mihai Valcu <valcu@orn.mpg.de>
2025-11-28 18:48:11 +0100

    align to downstream changes.

--- R/map.R
+++ R/map.R
@@ -1,15 +1,12 @@
-
 info_box <- function(items) {
-
-  html_list = 
+  html_list =
     sapply(items, function(item) glue::glue("<li>{item}</li>")) |>
     glue_collapse()
   html_list = glue::glue("<ul>{html_list}</ul>")
 
-
   htmltools::HTML(
     glue::glue(
-    '<div class="modal" id="infobox" tabindex="-1" role="dialog">
+      '<div class="modal" id="infobox" tabindex="-1" role="dialog">
         <div class="modal-dialog" role="document">
           <div class="modal-content">
             <div class="modal-body">
@@ -25,15 +22,15 @@ info_box <- function(items) {
 #' Map a ctdf object
 #'
 #' Visualize clustered track data on an interactive map.
-#' 
+#'
 #' @param ctdf A `ctdf` object produced by `cluster_track()`.
-#' @param path Optional `character`. File path where the HTML map will be saved. 
+#' @param path Optional `character`. File path where the HTML map will be saved.
 #'             If omitted, the function returns the `leaflet` map object.
-#' @param prop Numeric (0 < prop ≤ 1). Fraction of each cluster’s points 
-#'            used to construct the site polygons on the map. 
-#'            lower values produce tighter, core‐area convex‐hull polygons; 
+#' @param prop Numeric (0 < prop ≤ 1). Fraction of each cluster’s points
+#'            used to construct the site polygons on the map.
+#'            lower values produce tighter, core‐area convex‐hull polygons;
 #'            prop = 1 (default) includes all points in the hull.
-#' @return A `leaflet` map object if `path` is not provided. 
+#' @return A `leaflet` map object if `path` is not provided.
 #'        If `path` is provided, an HTML file (and its `maplibs/` directory) is written to disk.
 #' @seealso [clusterTrack::cluster_track()], [htmlwidgets::saveWidget()]
 #
@@ -47,73 +44,90 @@ info_box <- function(items) {
 #' map(ctdf)
 
 map <- function(ctdf, path, prop = 1, fix_dateline = FALSE) {
-
-  
   clusterTrack:::.check_ctdf(ctdf)
 
-  if(nrow(ctdf[!is.na(cluster)])==0) stop("this ctdf does not have any clusters!")
+  if (nrow(ctdf[!is.na(cluster)]) == 0) {
+    stop("this ctdf does not have any clusters!")
+  }
 
-
-  N = glue("<b>N</b>:<br>
+  N = glue(
+    "<b>N</b>:<br>
             - fixes:{nrow(ctdf)} <br>
-            - segments: {max(ctdf$.segment, na.rm = TRUE)} <br>
-            - clusters: {max(ctdf$cluster)}")
-  
+            - segments: {max(ctdf$.putative_cluster, na.rm = TRUE)} <br>
+            - clusters: {max(ctdf$cluster)}"
+  )
+
   cluster_par = attr(ctdf, "cluster_params")
-  cluster_par = glue(" - {names(cluster_par)} = {cluster_par}") |> paste(collapse = "<br>")
+  cluster_par = glue(" - {names(cluster_par)} = {cluster_par}") |>
+    paste(collapse = "<br>")
   cluster_par = glue("<b>Parameters</b>:<br>{cluster_par}")
-  
+
   pack = glue("<i>clusterTrack v.{ packageVersion('clusterTrack')}</i>")
 
   nfo = c(N, cluster_par, pack)
 
   CD = st_as_sf(ctdf) |> st_transform("OGC:CRS84")
-  
-  if(fix_dateline) CD = .fix_dateline(CD)
-  
-  CD = mutate(CD,
-    pt_lab = glue("time: {format(timestamp, '%d-%b-%y %H:%M')} <br/> seg: {`.segment`}<b/r>")
+
+  if (fix_dateline) {
+    CD = .fix_dateline(CD)
+  }
+
+  CD = mutate(
+    CD,
+    pt_lab = glue(
+      "time: {format(timestamp, '%d-%b-%y %H:%M')} <br/> seg: {`.putative_cluster`}<b/r>"
+    )
   ) |>
     rowwise() |>
-    mutate(pt_lab = htmltools::HTML(pt_lab) )
+    mutate(pt_lab = htmltools::HTML(pt_lab))
 
-  move_points = dplyr::filter(CD, cluster ==0)  
-  site_points = dplyr::filter(CD, cluster != 0)  
-  
-    
+  move_points = dplyr::filter(CD, cluster == 0)
+  site_points = dplyr::filter(CD, cluster != 0)
+
   sites = summarise_ctdf(ctdf, prop = prop)
   sites[tenure < 1, Tenure := glue_data(.SD, "{round(tenure*24)}[h]")]
   sites[tenure > 1, Tenure := glue_data(.SD, "{round(tenure,1)}[d]")]
-  sites[, lab := glue_data(
-    .SD,
-    "tenure:{Tenure}                      <br/>
+  sites[,
+    lab := glue_data(
+      .SD,
+      "tenure:{Tenure}                       <br/>
     start:{format(start, '%d-%b-%y %Hh')} <br/>
     stop:{format(stop, '%d-%b-%y %Hh')}   <br/>
-    segments:{segments}                   <br/>
+    segments:{putative_clusters}           <br/>
     N:{N}"
-  )]
+    )
+  ]
 
   all_track = as_ctdf_track(ctdf) |> st_transform(crs = "OGC:CRS84") |> setDT()
-  all_track[, let(segement = factor(.segment) )]
+  all_track[, let(segement = factor(.putative_cluster))]
   all_track = st_as_sf(all_track)
-  if(fix_dateline) all_track = .fix_dateline(all_track)
+  if (fix_dateline) {
+    all_track = .fix_dateline(all_track)
+  }
 
   polys = sites[, .(cluster, site_poly)] |> st_as_sf()
-  if(fix_dateline) polys = .fix_dateline(polys)
-  
+  if (fix_dateline) {
+    polys = .fix_dateline(polys)
+  }
+
   labels = sites[, .(cluster, lab, site_poly_center, stop)] |> st_as_sf()
-  if(fix_dateline) labels = .fix_dateline(labels)
+  if (fix_dateline) {
+    labels = .fix_dateline(labels)
+  }
 
   # map elements
-    pal = colorFactor(viridis::viridis(unique(sites$cluster) |> length()), sites$cluster)
+  pal = colorFactor(
+    viridis::viridis(unique(sites$cluster) |> length()),
+    sites$cluster
+  )
 
-    clust_ico = awesomeIcons(
-      icon       = NULL,
-      text       = as.character(sites$cluster),
-      iconColor  = "black",
-      library    = "fa",
-      markerColor = "white"
-    )
+  clust_ico = awesomeIcons(
+    icon = NULL,
+    text = as.character(sites$cluster),
+    iconColor = "black",
+    library = "fa",
+    markerColor = "white"
+  )
 
   # build map
   mm =
@@ -121,39 +135,42 @@ map <- function(ctdf, path, prop = 1, fix_dateline = FALSE) {
     addTiles(group = "OSM Default") |>
     addProviderTiles("OpenTopoMap", group = "Open Topo Map") |>
     addProviderTiles("Esri.WorldImagery", group = "Esri World Imagery") |>
-    addProviderTiles("Esri.WorldGrayCanvas", group = "Esri World Gray Canvas") |>
+    addProviderTiles(
+      "Esri.WorldGrayCanvas",
+      group = "Esri World Gray Canvas"
+    ) |>
     addPolylines(
-      data    = all_track,
-      color   = "#7e7f81cc",
-      weight  = 1.5,
+      data = all_track,
+      color = "#7e7f81cc",
+      weight = 1.5,
       opacity = 0.5,
-      group   = "All Track"
+      group = "All Track"
     ) |>
     addCircleMarkers(
-      data        = move_points,
-      label       = ~pt_lab,
-      radius      = 4,
-      color       =  "#7e7f81cc",
-      stroke      = FALSE,
-      opacity     = 0.3,
+      data = move_points,
+      label = ~pt_lab,
+      radius = 4,
+      color = "#7e7f81cc",
+      stroke = FALSE,
+      opacity = 0.3,
       fillOpacity = 1,
-      group       = "Site Track"
+      group = "Site Track"
     ) |>
     addPolygons(
-      data        = polys,
-      fillColor   = ~ pal(cluster),
+      data = polys,
+      fillColor = ~ pal(cluster),
       fillOpacity = 0.5,
-      weight      = 0,
-      group       = "Clusters"
+      weight = 0,
+      group = "Clusters"
     ) |>
     addCircleMarkers(
-      data        = site_points,
-      label       = ~pt_lab,
-      radius      = 5,
-      color       = ~ pal(cluster),
-      stroke      = FALSE,
+      data = site_points,
+      label = ~pt_lab,
+      radius = 5,
+      color = ~ pal(cluster),
+      stroke = FALSE,
       fillOpacity = 1,
-      group       = "Site Track"
+      group = "Site Track"
     ) |>
     addAwesomeMarkers(
       data = labels,
@@ -161,9 +178,9 @@ map <- function(ctdf, path, prop = 1, fix_dateline = FALSE) {
       popup = ~lab,
       group = "Cluster Icons",
       popupOptions = popupOptions(
-        autoPan      = FALSE,
-        autoClose    = FALSE,
-        keepInView   = TRUE,
+        autoPan = FALSE,
+        autoClose = FALSE,
+        keepInView = TRUE,
         closeOnClick = FALSE
       )
     ) |>
@@ -171,49 +188,53 @@ map <- function(ctdf, path, prop = 1, fix_dateline = FALSE) {
       #
     ) |>
     addEasyButton(easyButton(
-      icon    = '<i class="fa fa-dot-circle-o" style="color:red; font-weight:bold;"></i>',
-      title   = "clusterTrack",
+      icon = '<i class="fa fa-dot-circle-o" style="color:red; font-weight:bold;"></i>',
+      title = "clusterTrack",
       onClick = JS("function(btn, map){ $('#infobox').modal('show'); }")
     )) |>
     addTimeslider(
       data = labels,
       options = timesliderOptions(
         timeAttribute = "stop",
-         range        = FALSE,
-          rezoom      = FALSE
-        )
+        range = FALSE,
+        rezoom = FALSE
+      )
     ) |>
     appendContent(
       info_box(nfo)
     ) |>
     addLayersControl(
-      baseGroups    = c(
-                      "Esri World Gray Canvas",
-                      "Open Topo Map",
-                      "OSM Default",
-                      "Esri World Imagery"),
-      options       = layersControlOptions(collapsed = TRUE)
+      baseGroups = c(
+        "Esri World Gray Canvas",
+        "Open Topo Map",
+        "OSM Default",
+        "Esri World Imagery"
+      ),
+      options = layersControlOptions(collapsed = TRUE)
     )
 
-    if("true_cluster" %in% names(CD))
-      mm = 
+  if ("true_cluster" %in% names(CD)) {
+    mm =
       mm |>
       addCircleMarkers(
-        data        = dplyr::filter(CD, true_cluster>0),
-        radius      = 6,
-        color       = ~ "#e75d01",
+        data = dplyr::filter(CD, true_cluster > 0),
+        radius = 6,
+        color = ~"#e75d01",
         fillOpacity = 0,
-        weight      = 1.5,
-        label       =  ~paste("true clust:", true_cluster)
+        weight = 1.5,
+        label = ~ paste("true clust:", true_cluster)
       )
+  }
 
-
-
-  if(!missing(path)) {
-    saveWidget(mm, path,title = basename(path) |> str_remove('.html'),  selfcontained = FALSE, libdir = "maplibs")
+  if (!missing(path)) {
+    saveWidget(
+      mm,
+      path,
+      title = basename(path) |> str_remove('.html'),
+      selfcontained = FALSE,
+      libdir = "maplibs"
+    )
   } else {
     return(mm)
   }
-
-
 }
